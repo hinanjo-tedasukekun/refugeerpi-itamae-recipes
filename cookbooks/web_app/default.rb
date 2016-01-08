21ee183588f28354b6541aed8e5f9bf4dfd85467
Kosuke Yamashita <ochaochaocha3@gmail.com>
2016-01-08 20:32:43 +0900

    Web アプリ関連のサービス、ホスト設定を追加する

--- cookbooks/web_app/default.rb
+++ cookbooks/web_app/default.rb
@@ -1,5 +1,11 @@
 # Web アプリ関連レシピ
 
+package 'nginx'
 package 'firebird2.5-superclassic'
 package 'firebird-dev'
 package 'nodejs'
+
+remote_file '/etc/systemd/system/nginx.service'
+remote_file '/etc/systemd/system/refugee-webapp.service'
+remote_file '/etc/systemd/system/refugee-input-server.service'
+remote_file '/etc/systemd/system/refugee-com-server.service'
