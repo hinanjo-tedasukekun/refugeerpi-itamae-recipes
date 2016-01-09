dcacdc85e074f3796c6ef990084c03a1415c8d95
ocha <ochaochaocha3@gmail.com>
2016-01-10 08:34:11 +0900

    rbenv 関連の設定・ファイルを追加する

--- cookbooks/ruby/default.rb
+++ cookbooks/ruby/default.rb
@@ -11,3 +11,25 @@ package 'libncurses5-dev'
 package 'libffi-dev'
 package 'libgdbm3'
 package 'libgdbm-dev'
+
+git '/usr/local/rbenv' do
+  repository 'https://github.com/rbenv/rbenv.git'
+end
+
+remote_file '/etc/profile.d/rbenv.sh'
+
+directory '/usr/local/rbenv/plugins'
+
+git '/usr/local/rbenv/plugins/ruby-build' do
+  repository 'https://github.com/rbenv/ruby-build.git'
+end
+
+git '/usr/local/rbenv/plugins/rbenv-update' do
+  repository 'https://github.com/rkh/rbenv-update.git'
+end
+
+git '/usr/local/rbenv/plugins/rbenv-default-gems' do
+  repository 'https://github.com/rbenv/rbenv-default-gems.git'
+end
+
+remote_file '/usr/local/rbenv/default-gems'
