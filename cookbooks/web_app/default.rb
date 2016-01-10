b3db92007d591b378dac5b623ef336c3b23194d9
ocha <ochaochaocha3@gmail.com>
2016-01-10 14:00:44 +0900

    Nginx の設定ファイルを追加する

--- cookbooks/web_app/default.rb
+++ cookbooks/web_app/default.rb
@@ -7,6 +7,12 @@ package 'firebird2.5-superclassic'
 package 'firebird-dev'
 package 'nodejs'
 
+remote_file '/etc/nginx/sites-available/refugee_manager'
+
+link '/etc/nginx/sites-enabled/refugee_manager' do
+  to '../sites-available/refugee_manager'
+end
+
 remote_file '/etc/systemd/system/nginx.service'
 remote_file '/etc/systemd/system/refugee-webapp.service'
 remote_file '/etc/systemd/system/refugee-input-server.service'
